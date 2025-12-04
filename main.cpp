5f67d682ff1f6393ceef0f5f6e1ff7caf109b176
lxquaver <bernard.luca1206@gmail.com>
2025-12-04 11:19:02 +0100

    Ajout du game.h pour et meilleure gestion du nom des players

--- main.cpp
+++ main.cpp
@@ -1,25 +1,16 @@
+#include "Player.h"
 #include <iostream>
-#include <array>
-#include "Square.h"
-#include "Column.h"
-#include "Grid.h"
-#include "player.h"
 #include "SelectName.h"
+#include "Game.h"
 
 using namespace std;
 
 string nom = AskName();
 
 int main() {
-    Player player1(nom);
-    cout << "Bonjour, " << player1.getNom() << "!" << endl;
-    system("pause");
-    Player player2(nom);
-    cout << "Bonjour, " << player2.getNom() << "!" << endl;
-    system("pause");
-    Grid grid;
-    grid.display();
-    system("pause");
+    Game game;
+    game.SelectNames();
     return 0;
 
+    
 }
